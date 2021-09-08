f9b2d533496438b2d324cbf6a356ffed850eb40c
Barba <Barba@192.168.0.234>
2021-09-08 12:39:54 -0300

    cambio

--- src/poo/Guitarra.java
+++ src/poo/Guitarra.java
@@ -1,4 +1,4 @@
-/*
+/* hola
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -14,5 +14,5 @@ class Guitarra extends Instrumento {
         System.out.println("Guitarra.tocar()");
 }
     public String tipo() { return "Guitarra"; }
-        public void afinar() {}
+        public void afinar() {}//anda a la canche bobo
 }
